ddc13c9ef97cf54319da2704caa50ac5deb43c1e
Noemi Pezzati <npezzati@c1r5p3.42firenze.it>
2024-11-26 15:53:50 +0100

    parte bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npezzati <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 15:42:26 by npezzati          #+#    #+#             */
+/*   Updated: 2024/11/26 15:42:30 by npezzati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+	
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	
+	return (new_node);
+}
--- libft.h
+++ libft.h
@@ -19,6 +19,12 @@
 # include <stdio.h>
 //# include <string.h>
 
+typedef struct s_list
+{
+void *content;
+struct s_list *next;
+} t_list;
+
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
